0335cd0587c2cb9c628e2e08f21a050b4a85d829
amanuela97 <babyzewdie@gmail.com>
2024-08-07 21:48:16 +0300

    feat: use webpackMerge with different config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpackMerge = require("webpack-merge");
-//const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
 
 module.exports = ({ mode } = { mode: "production", presets: [] }) => {
-  return {
-    mode,
-    output: {
-      filename: "bundle.js",
+  return webpackMerge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js",
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+    modeConfig(mode)
+  );
 };
